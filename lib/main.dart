8add9345c7bb4daaf3d50006e1b9e53e433d061f
Yazeed Gazalat <105671860+YazeedGhazalat@users.noreply.github.com>
2022-10-29 18:46:14 +0300

    this is 1st update

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:color_pages/HomePage.dart';
+import 'package:color_pages/info.dart';
+import 'package:color_pages/loginPage/login.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'firebase_options.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Home();
+          }
+          return Auth();
+        },
+      ),
+      routes: {
+        '/information': (context) => Information(),
+      },
+    );
+  }
+}
